b3f663d0f92c2e35f8e10a697f6b11e4899667ac
Nils Eriksson <nils.edvin.eriksson@outlook.com>
2019-06-12 21:01:19 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+
+/**
+ * This function takes an vector of cards
+ */
+fn poker_hand(table: &[usize]) {
+
+    /* */
+    for card in table {
+
+    }
+
+
+}
+
+fn main() {
+    
+    let v = vec![ 1, 3, 7];
+
+    poker_hand(&v);
+
+}

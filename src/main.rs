bcab24c8b8499b3f510afdad448ac1e51bf9581d
nils <nils@manjaro-virtual>
2019-07-29 21:39:25 +0200

    e

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 #![allow (dead_code)]
 
+extern crate enum_map;
+#[macro_use]
+
+use enum_map::EnumMap;
+use std::collections::HashMap;
+
 /* Lets start with a hand and work downwards */ 
 struct Hand {
 
     cards: Vec<Card>,
-    winning_cards: Vec<Card>,
     score: Score,
 
 }
 
 impl Hand {
     fn new(cards: Vec<Card>) -> Hand {
-
         Hand {
             cards,
-            score = Score::new(&cards),
-
+            score: Score::new(&cards),
         }
     }
 }
@@ -23,29 +26,63 @@ impl Hand {
 
 struct Card {
 
-    rank: Rank,
+    rank: u8,
     suit: Suit,
 
 }
 
 
-struct Score {
-    hand_type: HandType,
-    type_rank: u8,
+pub struct Score {
+
+    grade: u8,
+    r#type: HandType,
+    winning_hand: Vec<Card>,
+
 }
 
 impl Score {
+
     fn new(cards: &Vec<Card>) -> Score {
+
+        /* Return tupple of Suit { High, Pair, TwoPair, Trips, House, Quads, Five } */
+        let pair: Score = {
+
+            //let mut rank_counter = EnumMap::new();
+            let mut rank_counter = HashMap::new();
+
+            for card in cards {
+                
+            }
+
+            Score::new(cards: &Vec<Card>)
+
+
+        };
+
+        let straight: Score = {
+
+        };
+
+        let flush: Score = {
+
+        };
+
+
         Score {
 
-            
+            grade: ,
+            r#type: ,
+            winning_hand: ,
 
         }
+
     }
+
 }
 
 
 /* enum with implicit disciminator (Ace=0, ... , King=12, Joker=13),  */
+/* 
 enum Rank {
     Ace,
     Two,
@@ -63,6 +100,24 @@ enum Rank {
     Joker,
 
 }
+ */
+
+mod Rank {
+    const Ace   : u8 = 0;
+    const Two   : u8 = 1;
+    const Three : u8 = 2;
+    const Four  : u8 = 3;
+    const Five  : u8 = 4;
+    const Six   : u8 = 5;   
+    const Seven : u8 = 6;
+    const Eight : u8 = 7;
+    const Nine  : u8 = 8;
+    const Ten   : u8 = 9;   
+    const Jack  : u8 = 10;
+    const Queen : u8 = 11;
+    const King  : u8 = 12;
+    const Joker : u8 = 13;
+}
 
 
 enum Suit {
@@ -73,10 +128,6 @@ enum Suit {
     Spades,
     
 }
-
-
-
-
 
 
 /* implicit discriminator, higher score is better (duh...) */
@@ -100,5 +151,6 @@ enum HandType {
 
 fn main() {
     
+    let h = Hand::new(vec!(Card{ rank: Rank::Ace, suit: Suit::Clubs }));
 
 }

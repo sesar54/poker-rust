189b0518b3293a42dc5183cb83f9606660350ae2
Nils Eriksson <nils.edvin.eriksson@outlook.com>
2019-10-14 12:21:54 +0200

    Added more prelude and warnings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,26 @@
-// Modules
+//! A library for poker games
+#![warn(unused_import_braces, unused_qualifications, trivial_casts)]
+#![warn(trivial_numeric_casts, private_in_public, variant_size_differences)]
+#![warn(stable_features, unreachable_pub, non_shorthand_field_patterns)]
+#![warn(unused_attributes, unused_imports, unused_mut)] // missing_docs
+#![warn(renamed_and_removed_lints, stable_features, unused_allocation)]
+#![warn(unused_comparisons, bare_trait_objects, unused_must_use, const_err)]
+#![forbid(unsafe_code)]
+
+// Help Modules
+#[macro_use]
+pub mod macros;
+pub mod clump;
+
+// Basic Modules
 pub mod card;
 pub mod deck;
 
+// Game Modules
 pub mod holdem;
 
-#[macro_use]
-pub mod macros;
-
-pub mod clump;
-
 // Prelude
-pub use card::{ Suit::*, Value::*, * };
-
+pub use card::*;
+pub use deck::*;
+pub use macros::*;
 pub use clump::*;

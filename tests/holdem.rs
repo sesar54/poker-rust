a6135c45a25c99f884dfc4af8f4a3e8707af3ed3
Nils Eriksson <nils.edvin.eriksson@gmail.com>
2019-11-04 21:57:47 +0100

    Replaced test.

--- tests/holdem.rs
+++ tests/holdem.rs
@@ -4,53 +4,14 @@ extern crate dead_mans_hand as poker;
 mod hand {
 
     use poker::*;
-    use poker::holdem::Hand;
 
     #[test]
     fn test() {
 
-        // let cards = cards!(Ace, Spades; King, Spades; Queen, Diamonds; Jack, Clubs; Ten, Clubs; Nine, Spades; Eight, Spades; Seven, Spades);
+        for _ in 0..10000 {
 
-        let mut card_as_numb = Vec::<u8>::new();
-
-        fn overflow_clock(vec: &mut Vec<u8>, i: usize) {
-
-            if vec.len() == i {
-                vec.push(0);
-
-            } else {
-                vec[i] += 1;
-
-                if vec[i] > 52 {
-                    vec[i] = 0;
-
-                    if vec.len() -1 == i {
-                        vec.push(0)
-                    } else {
-                        vec[i+1] +=1;
-                        overflow_clock(vec, i+1);
-                    }
-
-                }
-
-
-            }
-        }
-
-        while card_as_numb.len() <= 5 {
-            overflow_clock(&mut card_as_numb, 0);
-
-            let mut cards: Vec<Card> = Vec::new();
-
-            for i in card_as_numb.iter() {
-                cards.push(Card::from(*i));
-            }
-
-            let hand = Hand::new(&cards);
-            println!("{:?}", hand);
-
+            Hand::new(Deck::new().deal(7).unwrap());
 
         }
-
     }
 }

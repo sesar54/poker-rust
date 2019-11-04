93e172aa74cf0c817e168f0c45a11d59078e6095
Nils Eriksson <nils.edvin.eriksson@gmail.com>
2019-11-04 17:11:47 +0100

    Implemented functions Card::new and from(u8) for enum Suit and Value.

--- src/card/impl.rs
+++ src/card/impl.rs
@@ -4,7 +4,14 @@ use std::cmp::Ordering::*;
 
 
 impl Card {
-  
+
+    pub fn new(value: Value, suit: Suit) -> Card {
+        Card {
+            value: value,
+            suit: suit,
+        }
+    }
+
     pub fn cmp_suit_first(&self, other: &Self) -> std::cmp::Ordering {
         match self.suit.cmp(&other.suit) {
             Equal => self.value.cmp(&other.value),
@@ -37,7 +44,6 @@ impl Into<u8> for Card {
     }
 }
 
-
 impl Suit {
     pub const SIZE: usize = 4;
 }
@@ -48,18 +54,58 @@ impl fmt::UpperHex for Suit {
     }
 }
 
-
 impl fmt::UpperHex for Value {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:X}", *self as u8)
     }
 }
 
-
-/*
 impl From<u8> for Card {
-    fn from(u8) -> Card {
+    fn from(u: u8) -> Card {
+        Card::new(Value::from(u), Suit::from(u))
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Impl Value and Suit enums                                                  //
+////////////////////////////////////////////////////////////////////////////////
+
+impl From<u8> for Suit {
+    fn from(u: u8) -> Suit {
+        match u % 4 {
+
+            0 => Suit::Clubs,
+            1 => Suit::Diamonds,
+            2 => Suit::Hearts,
+            3 => Suit::Spades,
+            _ => unreachable!()
+
+        }
+    }
+}
+
+impl From<u8> for Value {
+    fn from(u: u8) -> Value {
+        if u > 52 {
+            Value::Joker
+        } else {
+            match u % 13 {
+                0 =>    Value::Ace,
+                1 =>    Value::Two,
+                2 =>    Value::Three,
+                3 =>    Value::Four,
+                4 =>    Value::Five,
+                5 =>    Value::Six,
+                6 =>    Value::Seven,
+                7 =>    Value::Eight,
+                8 =>    Value::Nine,
+                9 =>    Value::Ten,
+                10 =>   Value::Jack,
+                11 =>   Value::Queen,
+                12 =>   Value::King,
+                _ => unreachable!()
+            }
+        }
 
     }
-}*/
-
+}
